72b65da8ff63b44a2502d856754947847bf39cf5
DataNick <DataNick@users.noreply.github.com>
2014-11-06 00:24:45 -0500

    Refactoring to save new films in database

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -2,53 +2,34 @@ class FilmsController < ApplicationController
 
 	def index 
 		@films = Film.all # @films are instances of all objects of film class?
-		# 	{
-		# 	   :title  => "Chungking Express",
-		# 	   :artist => "Wong Kar-Wai",
-		# 	   :url    => "http://www.craigskinnerfilm.com/wp-content/uploads/2012/02/Chungking-Express-GIF.gif"
-		# 	},
-		# 	{
-		# 	   :title  => "North by Northwest",
-		# 	   :artist => "Alfred Hitchcock",
-		# 	   :url    => "http://images5.fanpop.com/image/photos/31600000/North-By-Northwest-Gif-Image-north-by-northwest-31644733-500-272.gif"
-		# 	},
-		# 	{
-		# 	   :title  => "The Big Lebowski",
-		# 	   :artist => "The Coen Brothers",
-		# 	   :url    => "http://media3.giphy.com/media/x4vMKWXxkSA9i/giphy.gif"
-		# 	}
-		# ]
 
 	
 	end
 	
 	def show
 		@film = Film.find(params[:id]) # is @film an instance of a specific instance of film class?
-		# 	{
-		# 		:title  => "Chungking Express",
-		# 		:artist => "Wong Kar-Wai",
-		# 		:url    => "http://www.craigskinnerfilm.com/wp-content/uploads/2012/02/Chungking-Express-GIF.gif"
-		# 	},
-		# 	{
-		# 		:title  => "North by Northwest",
-		# 		:artist => "Alfred Hitchcock",
-		# 		:url    => "http://images5.fanpop.com/image/photos/31600000/North-By-Northwest-Gif-Image-north-by-northwest-31644733-500-272.gif"
-		# 	},
-		# 	{
-		# 		:title  => "The Big Lebowski",
-		# 		:artist => "The Coen Brothers",
-		# 		:url    => "http://media3.giphy.com/media/x4vMKWXxkSA9i/giphy.gif"
-		# 	}
-		# ]
+	
 		# @film = @films[params[:id].to_i]
 	end
 
 	def new
-		
+		@film = Film.new
 	end
 
 	def create
-		render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+		@film = Film.new(film_params)
+		if @film.save
+			redirect_to films_url
+		else
+			render :new
+		end
+		# render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+
+	end
+
+	private
+	def film_params
+		params.require(:film).permit(:artist, :title, :url)
 	end
 
 
