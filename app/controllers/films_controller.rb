bfa21cf93e714f61724e668041c43a4f394cb393
DataNick <DataNick@users.noreply.github.com>
2014-11-05 22:02:59 -0500

    Making films show at /films

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,6 +1,42 @@
 class FilmsController < ApplicationController
 
-	def index
-		
+	def index 
+		@films = [
+			{
+			   :title  => "Chungking Express",
+			   :artist => "Wong Kar-Wai",
+			   :url    => "http://www.craigskinnerfilm.com/wp-content/uploads/2012/02/Chungking-Express-GIF.gif"
+			},
+			{
+			   :title  => "North by Northwest",
+			   :artist => "Alfred Hitchcock",
+			   :url    => "http://images5.fanpop.com/image/photos/31600000/North-By-Northwest-Gif-Image-north-by-northwest-31644733-500-272.gif"
+			},
+			{
+			   :title  => "The Big Lebowski",
+			   :artist => "The Coen Brothers",
+			   :url    => "http://media3.giphy.com/media/x4vMKWXxkSA9i/giphy.gif"
+			}
+		]
+
+		# @films = [
+		#      {
+		#        :title  => "Chungking Express",
+		#        :artist => "Wong Kar-Wai",
+		#        :url    => "http://www.craigskinnerfilm.com/wp-content/uploads/2012/02/Chungking-Express-GIF.gif"
+		#      },
+		#      {
+		#        :title  => "The Big Lebowski",
+		#        :artist => "The Coen Brothers",
+		#        :url    => "http://media3.giphy.com/media/x4vMKWXxkSA9i/giphy.gif"
+		#      },
+		#      {
+		#        :title  => "North by Northwest",
+		#        :artist => "Alfred Hitchcock",
+		#        :url    => "http://images5.fanpop.com/image/photos/31600000/North-By-Northwest-Gif-Image-north-by-northwest-31644733-500-272.gif"
+		#      }
+		#    ]
+		#  end
 	end
+	
 end
