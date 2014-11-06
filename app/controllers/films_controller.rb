3aa2634e437c926f4df702c45fc598df62d68d56
DataNick <DataNick@users.noreply.github.com>
2014-11-05 23:59:13 -0500

    Moving @pictures in pictures_controller.rb into database

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,63 +1,46 @@
 class FilmsController < ApplicationController
 
 	def index 
-		@films = [
-			{
-			   :title  => "Chungking Express",
-			   :artist => "Wong Kar-Wai",
-			   :url    => "http://www.craigskinnerfilm.com/wp-content/uploads/2012/02/Chungking-Express-GIF.gif"
-			},
-			{
-			   :title  => "North by Northwest",
-			   :artist => "Alfred Hitchcock",
-			   :url    => "http://images5.fanpop.com/image/photos/31600000/North-By-Northwest-Gif-Image-north-by-northwest-31644733-500-272.gif"
-			},
-			{
-			   :title  => "The Big Lebowski",
-			   :artist => "The Coen Brothers",
-			   :url    => "http://media3.giphy.com/media/x4vMKWXxkSA9i/giphy.gif"
-			}
-		]
+		@films = Film.all # @films are instances of all objects of film class?
+		# 	{
+		# 	   :title  => "Chungking Express",
+		# 	   :artist => "Wong Kar-Wai",
+		# 	   :url    => "http://www.craigskinnerfilm.com/wp-content/uploads/2012/02/Chungking-Express-GIF.gif"
+		# 	},
+		# 	{
+		# 	   :title  => "North by Northwest",
+		# 	   :artist => "Alfred Hitchcock",
+		# 	   :url    => "http://images5.fanpop.com/image/photos/31600000/North-By-Northwest-Gif-Image-north-by-northwest-31644733-500-272.gif"
+		# 	},
+		# 	{
+		# 	   :title  => "The Big Lebowski",
+		# 	   :artist => "The Coen Brothers",
+		# 	   :url    => "http://media3.giphy.com/media/x4vMKWXxkSA9i/giphy.gif"
+		# 	}
+		# ]
 
-		# @films = [
-		#      {
-		#        :title  => "Chungking Express",
-		#        :artist => "Wong Kar-Wai",
-		#        :url    => "http://www.craigskinnerfilm.com/wp-content/uploads/2012/02/Chungking-Express-GIF.gif"
-		#      },
-		#      {
-		#        :title  => "The Big Lebowski",
-		#        :artist => "The Coen Brothers",
-		#        :url    => "http://media3.giphy.com/media/x4vMKWXxkSA9i/giphy.gif"
-		#      },
-		#      {
-		#        :title  => "North by Northwest",
-		#        :artist => "Alfred Hitchcock",
-		#        :url    => "http://images5.fanpop.com/image/photos/31600000/North-By-Northwest-Gif-Image-north-by-northwest-31644733-500-272.gif"
-		#      }
-		#    ]
-		#  end
+	
 	end
 	
 	def show
-		@films = [
-			{
-				:title  => "Chungking Express",
-				:artist => "Wong Kar-Wai",
-				:url    => "http://www.craigskinnerfilm.com/wp-content/uploads/2012/02/Chungking-Express-GIF.gif"
-			},
-			{
-				:title  => "North by Northwest",
-				:artist => "Alfred Hitchcock",
-				:url    => "http://images5.fanpop.com/image/photos/31600000/North-By-Northwest-Gif-Image-north-by-northwest-31644733-500-272.gif"
-			},
-			{
-				:title  => "The Big Lebowski",
-				:artist => "The Coen Brothers",
-				:url    => "http://media3.giphy.com/media/x4vMKWXxkSA9i/giphy.gif"
-			}
-		]
-		@film = @films[params[:id].to_i]
+		@film = Film.find(params[:id]) # is @film an instance of a specific instance of film class?
+		# 	{
+		# 		:title  => "Chungking Express",
+		# 		:artist => "Wong Kar-Wai",
+		# 		:url    => "http://www.craigskinnerfilm.com/wp-content/uploads/2012/02/Chungking-Express-GIF.gif"
+		# 	},
+		# 	{
+		# 		:title  => "North by Northwest",
+		# 		:artist => "Alfred Hitchcock",
+		# 		:url    => "http://images5.fanpop.com/image/photos/31600000/North-By-Northwest-Gif-Image-north-by-northwest-31644733-500-272.gif"
+		# 	},
+		# 	{
+		# 		:title  => "The Big Lebowski",
+		# 		:artist => "The Coen Brothers",
+		# 		:url    => "http://media3.giphy.com/media/x4vMKWXxkSA9i/giphy.gif"
+		# 	}
+		# ]
+		# @film = @films[params[:id].to_i]
 	end
 
 	def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Film.create!(
+	:title  => "Chungking Express",
+	:artist => "Wong Kar-Wai",
+	:url    => "http://www.craigskinnerfilm.com/wp-content/uploads/2012/02/Chungking-Express-GIF.gif"
+)
+
+Film.create!(
+  :title  => "North by Northwest",
+  :artist => "Alfred Hitchcock",
+  :url    => "http://images5.fanpop.com/image/photos/31600000/North-By-Northwest-Gif-Image-north-by-northwest-31644733-500-272.gif"
+)
+
+Film.create!(
+  :title  => "The Big Lebowski",
+  :artist => "The Coen Brothers",
+  :url    => "http://media3.giphy.com/media/x4vMKWXxkSA9i/giphy.gif"
+)
